90c74ddf1533a998a95c933a371584136ac1ce66
Shabierte <63858032+Shabierte2@users.noreply.github.com>
2021-05-04 14:04:36 +0200

    Pruebas de Orbe que ataca

---
+++ Assets/Scripts/Enemies/Orbs/FSM_AttackerOrb.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSM_AttackerOrb : MonoBehaviour
+{
+    public Transform lookAt1;
+    public Transform lookAt2;
+    
+
+    private void Start()
+    {
+
+        
+    }
+
+    private void Update()
+    {
+       
+        
+        
+     
+    }
+
+    
+}
